e6f8a0c73b2e5158603c6f0bca2e7d42b00107ef
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-05-19 11:03:21 +0300

    Migrate owner data from Flat to Owner

---
+++ property/migrations/0013_fill_owner_table.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+
+
+def fill_owners_from_flats(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    Owner = apps.get_model('property', 'Owner')
+    flats = Flat.objects.all()
+    for flat in flats:
+        Owner.objects.get_or_create(
+            name=flat.owner,
+            owner_pure_phone=flat.owner_pure_phone,
+            defaults={
+            'owners_phonenumber': flat.owners_phonenumber,
+        })
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        (('property', '0012_auto_20230517_2155'))
+    ]
+
+    operations = [
+        migrations.RunPython(fill_owners_from_flats)
+    ]
